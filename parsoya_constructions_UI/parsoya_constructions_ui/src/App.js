eae314719e4105d376b8baf167b92fd22117adaa
RaviPraswal <raviv8498@gmail.com>
2023-03-08 18:12:39 +0530

    react new ui setup

---
+++ parsoya_constructions_UI/parsoya_constructions_ui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListUserComponents from './ListUserComponents';
+import HomeSection from './HomeSection';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ListUserComponents/> */}
+      <HomeSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ parsoya_constructions_UI/parsoya_constructions_ui/src/HomeSection.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeSection extends Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+export default HomeSection;
