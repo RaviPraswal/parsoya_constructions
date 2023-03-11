606850643c5606d236122620105707f1afc803a2
liferay12 <ashwani.liferay@gmail.com>
2023-03-11 17:50:17 +0530

    setted home background image

--- parsoya_constructions_UI/parsoya_constructions_ui/src/App.js
+++ parsoya_constructions_UI/parsoya_constructions_ui/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import ListUserComponents from './ListUserComponents';
-import HomeSection from './HomeSection';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import ListUserComponents from './ListUserComponents';
+import HomeSection from './component/HomeSection';
 
 function App() {
   return (
     <div className="App">
       {/* <ListUserComponents/> */}
+      {/* <p id="para"  >Ashwani rao</p> */}
       <HomeSection/>
     </div>
   );
